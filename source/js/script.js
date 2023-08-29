0c8aede6c4319fca796a2ce522ff063518137dfc
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-08-29 18:25:42 +0300

    изменяет скрипт

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,17 @@
 let nav = document.querySelector('.main-navigation');
-let siteList = document.querySelector('.site-list');
-let navToggle = document.querySelector('.main-header__toggle');
+let navList = document.querySelector('.nav-list');
+let buttonNavToggle = document.querySelector('.main-header__toggle');
 let logo = document.querySelector('.main-header__logo');
 let header = document.querySelector('.main-header');
 
 nav.classList.remove('main-navigation--nojs');
 logo.classList.remove('main-header__logo--nojs');
-navToggle.classList.remove('main-header__toggle--nojs');
-siteList.classList.remove('site-list--opened');
+buttonNavToggle.classList.remove('main-header__toggle--nojs');
+navList.classList.remove('nav-list--opened');
 header.classList.remove('main-header--nojs');
 
-navToggle.addEventListener('click', () => {
+buttonNavToggle.addEventListener('click', () => {
   nav.classList.toggle('main-navigation--closed');
-  navToggle.classList.toggle('main-header__toggle--opened');
-  siteList.classList.toggle('site-list--opened');
+  buttonNavToggle.classList.toggle('main-header__toggle--opened');
+  navList.classList.toggle('nav-list--opened');
 });
