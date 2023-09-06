5247dabb07fa66940b7c9237e8897008479c66e2
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-09-06 23:04:10 +0300

    подключает скрипты

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,6 @@
-let nav = document.querySelector('.main-navigation');
-let navList = document.querySelector('.nav-list');
-let buttonNavToggle = document.querySelector('.main-header__toggle');
-let logo = document.querySelector('.main-header__logo');
-let header = document.querySelector('.main-header');
+import './main-header.js';
+import './pristine.min.js';
+import {setOnFormSubmit, resetForm} from './form-validation.js';
+import './modal-message.js';
 
-nav.classList.remove('main-navigation--nojs');
-logo.classList.remove('main-header__logo--nojs');
-buttonNavToggle.classList.remove('main-header__toggle--nojs');
-navList.classList.remove('nav-list--opened');
-header.classList.remove('main-header--nojs');
-
-buttonNavToggle.addEventListener('click', () => {
-  nav.classList.toggle('main-navigation--closed');
-  buttonNavToggle.classList.toggle('main-header__toggle--opened');
-  navList.classList.toggle('nav-list--opened');
-});
+setOnFormSubmit(resetForm);
